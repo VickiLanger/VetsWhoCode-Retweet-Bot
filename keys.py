66dfb5f22bab96b7a5b850dc846aa33949174853
Vicki <vicki.langer@statalog.org>
2019-10-31 13:12:08 -0400

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
 # Twitter application consumer keys and access tokens.
 # Remember, keep this information private.
-consumer_key = 'XXXXXXXXXX'
-consumer_secret = 'XXXXXXXXXX'
-access_token = 'XXXXXXXXXX'
-access_token_secret = 'XXXXXXXXXX'
+consumer_key = 'Y3Kvru7ZgXK6z1vGkS4JAEncz'
+consumer_secret = '8dbczn7YyRfAXv6PL5g9tExehNsgPQzBKfxUkhIcaC2LERvUP2'
+access_token = '703775726365966336-3lEsnrnsqVdZ5tpoWjnEkC11RxaLEyn'
+access_token_secret = '0vw4sloih3w4LJ2J4TNlOwukJKTi4iBbb6642H9nbKh0a'
