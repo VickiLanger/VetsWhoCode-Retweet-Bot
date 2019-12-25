d3ad7622e793860cca7585f2878b69feed0aa6ca
Vicki <46823403+VickiLanger@users.noreply.github.com>
2019-12-24 23:31:07 -0500

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
 # Twitter application consumer keys and access tokens.
 # Remember, keep this information private.
-consumer_key = 'Y3Kvru7ZgXK6z1vGkS4JAEncz'
-consumer_secret = '8dbczn7YyRfAXv6PL5g9tExehNsgPQzBKfxUkhIcaC2LERvUP2'
-access_token = '703775726365966336-3lEsnrnsqVdZ5tpoWjnEkC11RxaLEyn'
-access_token_secret = '0vw4sloih3w4LJ2J4TNlOwukJKTi4iBbb6642H9nbKh0a'
+CONSUMER_KEY = 'consumer key'
+CONSUMER_SECRET = 'consumer secret'
+ACCESS_KEY = 'access key'
+ACCESS_SECRET = 'access secret'
