653ab8779798806092c266543e17eaeb49f25a3a
Vicki <vicki.langer@statalog.org>
2019-12-26 17:14:13 -0500

    Update retweet.py

--- retweet.py
+++ retweet.py
@@ -1,43 +1,40 @@
-# Retweet bot for Twitter, using Python and Tweepy.
-# Search query via hashtag or keyword.
-# Author: Tyler L. Jones || CyberVox
-# Date: Saturday, May 20th - 2017.
-# License: MIT License.
 '''
 retweet.py: updated to be #VetsWhoCode bot
-31 October 2019
-Vicki Langer
+26 December 2019
+Vicki Langer (@vicki_langer)
 '''
 
 import tweepy
 from time import sleep
-# Import in your Twitter application keys, tokens, and secrets.
-# Make sure your keys.py file lives in the same directory as this .py file.
+
 from keys import *
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# Where q='#example', change #example to whatever hashtag or keyword you want to search.
-# Where items(5), change 5 to the amount of retweets you want to tweet.
-# Make sure you read Twitter's rules on automation - don't spam!
-for tweet in tweepy.Cursor(api.search, q='#vetswhocode').items(5):
-    try:
-        print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
 
-        tweet.retweet()
-        print('Retweet published successfully.')
+user = api.me()
+print(user.name)
 
-        # Where sleep(10), sleep is measured in seconds.
-        # Change 10 to amount of seconds you want to have in-between retweets.
-        # Read Twitter's rules on automation. Don't spam!
-        sleep(10)
+def main():
+    search_term = ("#VetsWhoCode", "#vetsWhoCode", "#VeteransWhoCode", "#veteransWhoCode")
+    number_of_tweets = 5
 
-    # Some basic error handling. Will print out why retweet failed, into your terminal.
-    except tweepy.TweepError as error:
-        print('\nError. Retweet not successful. Reason: ')
-        print(error.reason)
+    for tweet in tweepy.Cursor(api.search, search_term).items(number_of_tweets):
+        try:
+            print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
+            tweet.retweet()
+            print("Yay! Tweet was retweeted :)")
+            
+            #sleep is measured in seconds
+            sleep(10)
 
-    except StopIteration:
-        break
+        except tweepy.TweepError as error:
+            print(error.reason)
+
+        except StopIteration:
+            break
+
+main()
+
