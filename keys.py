39983320ffa303d22dafdbc7a18fccefa4a9c2c3
Vicki <vicki.langer@statalog.org>
2019-12-26 23:10:25 -0500

    Delete keys.py

--- keys.py
+++
@@ -1,6 +0,0 @@
-# Twitter application consumer keys and access tokens.
-# Remember, keep this information private.
-CONSUMER_KEY = 'consumer key'
-CONSUMER_SECRET = 'consumer secret'
-ACCESS_KEY = 'access key'
-ACCESS_SECRET = 'access secret'
